ee58b2e270abfc8b207f3f0aa1ef5179e5143a5d
Barrett Kowalsky <barrettkowalsky@gmail.com>
2024-10-30 18:02:09 -0600

    update comments with more info

--- ch2/test_structure.py
+++ ch2/test_structure.py
@@ -3,12 +3,15 @@ from cards import Card
 
 def test_to_dict():
     # GIVEN a Card object with known contents
+    # Given/Arrange - a starting state. This is where you set up data or the environment to get ready for the action.
     c1 = Card("something", "brian", "todo", 123)
 
     # WHEN we call to_dict() on the object
+    # When/Act - some action is performed. This is the focus of the test - the behavior we are trying to make sure is working right.
     c2 = c1.to_dict()
 
     # THEN the result will be a dictionary with known content
+    # Then/Assert = some expected result or end state should happen. At the end of the test, we make sure the action resulted in the expected behavior.
     c2_expected = {
         "summary": "something",
         "owner": "brian",
